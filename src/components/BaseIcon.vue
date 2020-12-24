bae4f0e13d5e41ade7a0807d0fc3bf47b238aed8
niushae <fabiana.moreno.fm@gmail.com>
2020-12-24 07:04:01 -0400

    add Icon wrapper

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <component :is="iconComponent" role="img" />
+</template>
+
+<script>
+import Vue from "vue";
+
+const icons = {
+  trash: require("./icons/TrashIcon.vue").default,
+  edit: require("./icons/EditIcon.vue").default,
+  roundCheck: require("./icons/RoundCheckIcon.vue").default,
+  check: require("./icons/CheckIcon.vue").default,
+  add: require("./icons/AddIcon.vue").default
+};
+
+export default Vue.extend({
+  name: "BaseIcon",
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator(value) {
+        return Object.prototype.hasOwnProperty.call(icons, value);
+      }
+    }
+  },
+
+  computed: {
+    iconComponent: function() {
+      return icons[this.name];
+    }
+  }
+});
+</script>
